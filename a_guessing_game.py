eb3a25c7502019b3ad017fb0847edbf7514c4a6b
Oluwafunmilayo Somoye <32137766+FunmiSomoye@users.noreply.github.com>
2020-10-17 22:33:15 +0100

    Add files via upload
    Better Commenting

--- a_guessing_game.py
+++ a_guessing_game.py
@@ -11,112 +11,123 @@ tell them their guess is either too high, or too low.
 # imports
 #from random import seed # to ensure that your code produces the same result each time
 from random import randint # so to generate only integer values
-import sys
+import sys # import system module
 
 
-# initial values
-game_on = False
+game_on = False # initialise game_on as False
 
-# try a while loop
+# function for main program
+def main():
+    # welcome message
+    print("Welcome to the Guessing Program")
+
+    # instructions
+    print("Your computer would generate random numbers between 0 and 20. \nYou are to guess what number your computer generates. \nYou have a maximum of 10 guesses in one round")
+
+    # init
+    #seed(1) # seed the random number generator to increase predictability during testing
+    
+    # computer to generate random integers between 0 and 20
+    global computer # declare as a global variable
+    computer = randint(0, 20) 
+
+    # let the game begin
+    print("\nAre you ready to start?") # print on a new line
+    switch_on_game() # run the function to switch on the game
+
+
+# function to switch game on or off
+def switch_on_game():
+    global game_on # declare as a global variable
+    while True:
+        #try: # try exceptions
+            valid_response = ["yes", "no"] # set what a valid response looks like
+            game_play = input("Yes or No ?") # receive the user input
+            game_play = game_play.lower() # set user input to lower case letters
+            
+            # check if user input is a string
+            if (not game_play.isalpha()): 
+                print("Type in alphabets ('Yes' or 'No') ")
+            
+            # confirm that user entered yes or no
+            elif (game_play not in valid_response): 
+                print("Please type in 'Yes' or 'No'")
+
+            else:
+                # if Yes
+                if game_play == "yes": 
+                    game_on = True # initialise game_on as True
+                    print("\nWe hope you win the computer :)")
+                    run_game() # run the function to make the game run
+                    break # don't come back in here
+                
+                # if No
+                else:
+                    print("Goodbye")
+                    sys.exit() # exit the program
+    
+        #except ValueError: # raise a value error 
+           # print("\nPlease enter 'Yes' or 'No'.")
+            #continue
+
+
+# function to run the game
 def run_game():
 
-    global game_on
+    global game_on # declare as a global variable
    # game_on = game_on
 
-    while game_on:
-        count = 0
-        max_guesses = 10
+    while game_on: 
+        count = 0 # initialise count to monitor While loop
+        max_guesses = 10 # set maximum number of guesses to 10
 
         while count < max_guesses:
-            compare_values(validate_number_input(), computer)
-            count += 1  
+            # run the function to compare User guesses with Computer generated numbers
+            compare_values(validate_number_input(), computer) # run the compare values function
+            count += 1 # increase count
 
         print("\nYou have exceeded the maximum of {} guesses".format(max_guesses))
-        game_on = False
+        game_on = False # set game_on to False because maximum number of guesses has been exited
+
+        # give the user an option to play again if they want to play again
         print("\nWould you like to play again?")
-        switch_on_game() #switch on the game
+        switch_on_game() # run function to switch on the game
 
 
 # function to validate user input
 def validate_number_input():
      
-    global user_input
+    global user_input # declare as global variable
 
     while True: 
-        try: 
-            user_input = input("\nEnter a 'whole number' ")
-            user_input = int(user_input)
+        try: # use try except to validate user input
+            user_input = input("\nEnter a 'whole number' ") # receive the user input
+            user_input = int(user_input) # attempt to convert the user input to an integer
             
-            if user_input > 20:
+            if user_input > 20: 
                 print("\nPlease enter a number less than 20.")
-                continue
-            return user_input
+                continue # continue to the next line
+            return user_input # save user_input value into the function for future use
 
-        except ValueError:
+        except ValueError: # raise a value error if input is not an integer
             print("\nPlease enter positive whole numbers only, no letters or decimals either.")
-            continue
-    
 
 
-# function to switch game on or off
-def switch_on_game():
-    global game_on
-    while True:
-        try:
-            valid_response = ['yes', 'no']
-            game_play = input("Yes or No ?")
-            game_play = game_play.lower()
-            
-            if (not game_play.isalpha()):
-                print("Type in alphabets ('Yes' or 'No') ")
-                continue
-            
-            elif (game_play not in valid_response):
-                print("Please type in 'Yes' or 'No'")
-                continue
-
-            else:
-                if game_play == "yes":
-                    game_on = True
-                    print("\nWe hope you win the computer :)")
-                    run_game()
-                else:
-                    print("Goodbye")
-                    sys.exit()
-
-        except ValueError:
-            print("\nPlease enter 'Yes' or 'No'.")
-            continue
-
-
-# function to compare the numbers
+# function to compare the User value to the computer geenrated value
 def compare_values(user, computer):
     if user < computer:
         print("\nAwww, your guess is too low!")
-        #print("\nThe computer generated number {}".format(computer))
+        #print("\nThe computer generated number {}".format(computer)) # uncomment for testing
     elif user == computer:
         print("\nAwesome! You guessed correctly!!!")
         print("\nWould you like to play again?")
-        switch_on_game()
+        switch_on_game() # run the function to switch on the game
     else:
        print("\nYour guess is too high! Haha!")
-       #print("\nThe computer generated number {}".format(computer))
-    #return result
+       #print("\nThe computer generated number {}".format(computer)) # uncomment for testing
+    
 
-
-def main():
-    # program start
-    print("Welcome to the Guessing Program \nYou have a maximum of 10 guesses")
-
-    # init
-    #seed(1) # seed the random number generator to increase predictability during testing
-    computer = randint(0, 20)
-
-    # let the game begin
-    print("\nAre you ready to start?") 
-    switch_on_game()
-
-
+# start the program
 main()
 
 
--- secure_password_generator.py
+++ secure_password_generator.py
@@ -11,19 +11,29 @@ The password should be a minimum of 6 characters long.
 """
 
 import secrets # use secrets as it is said to be cryptographically safer than random 
-import string
-import random
+import string # import string module
+import random # import random module
 
 # Define Functions
+
+# function for main program
+def main():
+    # Action
+    validate_number_input() # run function to validate user input
+    print("\nHere is your password: ", generate_strong_password(length, num_letters)) # run function to generate secure password and print result
+
+# function to validate the user's input
 def validate_number_input():
     while True:
-        global length, num_letters
-        try:
-            length = input("How long do you want your password to be (character length)?")
-            num_letters = input("How many characters should be made of letters in the password?")
+
+        global length, num_letters # declare as global variables so they can be reused outside the function
+
+        try: # use try except to validate user input
+            length = input("How long do you want your password to be (character length)?") # length of password
+            num_letters = input("How many alphabets do you want in your password?") # number of alphabets in password
             
-            length = int(length)
-            num_letters = int(num_letters)
+            length = int(length) # attempt to convert the user input to an integer
+            num_letters = int(num_letters) # attempt to convert the user input to an integer
         
             if length < 6:
                 print("\nPlease enter a number greater than 6 for the length of your password.")
@@ -35,25 +45,23 @@ def validate_number_input():
 
         break
 
-
+# function to generate the secure password
 def generate_strong_password(length, letters):
-    difference = length - letters
-    count = int(difference/2)
-    password = ''.join(secrets.SystemRandom().choice(string.ascii_letters) for _ in range(letters)) #include SystemRandom() for cryptographic safety
-    password += ''.join(secrets.SystemRandom().choice(string.digits) for _ in range(count))
-    password += ''.join(secrets.SystemRandom().choice(string.punctuation) for _ in range(count))
+    space_left = length - letters # get a figure for how many integers and symbols to be in the password
+    num_or_symbol = int(space_left/2) # set new variable for number of integer strings and symbols
+
+    # generate strings, both upper and lower case
+    password_raw = ''.join(secrets.SystemRandom().choice(string.ascii_letters) for _ in range(letters)) # include SystemRandom() for cryptographic safety
+    # generate digits as string too using and join to password
+    password_raw += ''.join(secrets.SystemRandom().choice(string.digits) for _ in range(num_or_symbol)) # join each randomly generated with no separator to existing variable
+    # generate symbols and joint to password
+    password_raw += ''.join(secrets.SystemRandom().choice(string.punctuation) for _ in range(num_or_symbol)) # join each randomly generated with no separator to existing variable
 
     # shuffle the randomly generated characters
-    password_list = list(password)
-    random.shuffle(password_list)
-    password = ''.join(password_list)
+    password_list = list(password_raw) # make password a list
+    random.shuffle(password_list) # shuffle the new list
+    password = ''.join(password_list) # unpack the shuffled list and save as a new variable
     return password
 
-
-def main():
-    # Action
-    validate_number_input()
-    print("\nHere is your password: ", generate_strong_password(length, num_letters))
-
-
+# start the program
 main()
